b4680f64b914aa75950f1e989a16cd021fb4ec6a
Vadim Drobinin <vadim@drobinin.com>
2016-10-17 20:07:29 +0300

    Add Constants

---
+++ Rainy/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Rainy
+//
+//  Created by Vadim Drobinin on 17/10/16.
+//  Copyright © 2016 Vadim Drobinin. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+  static let apiKey = "fd4416ac5e2a835bb625c2931937a94c"
+}
